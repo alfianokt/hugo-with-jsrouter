ad6bfa3b2116a9b826c75d9659006ae70da4a242
Alfian Oktafireza <alfianokt104@gmail.com>
2021-06-22 10:30:20 +0700

    feat: element to change

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ router
   .on('*', () => {
     fetch(window.location.pathname)
       .then(r => r.text())
-      .then(text => document.body.innerHTML = text);
+      .then(text => document.querySelector('html').innerHTML = text);
   });
 
 router.listen();
